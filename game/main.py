6f6808bb4148d491ab4eac8479880325eb9a5fe4
yadenmezi <Yarden.Mezi@mail.huji.ac.il>
2024-11-01 15:06:29 +0200

    refactor: all messages in one file

--- game/main.py
+++ game/main.py
@@ -3,6 +3,7 @@ from pyrogram.types import InlineKeyboardMarkup
 from game_runner import GamesRunner
 from pyrogram import Client, filters
 from game import SPLIT_SIGN
+import game_messages as msgs
 
 
 def read_config(file_path):
@@ -29,20 +30,20 @@ games_runner = GamesRunner()
 def _handle_card_picking(content, game, chat_id, client):
     card_txt = game.get_card(chat_id, int(content))
     client.send_message(
-        games_runner.get_chat_id(chat_id), "Someone chose card:" + card_txt
+        games_runner.get_chat_id(chat_id), "Someone picked card:" + card_txt
     )
     if game.did_all_players_picked():
         picker_id = game.get_turn_id()
         buttons = game.get_buttons(picker_id)
         client.send_message(
             picker_id,
-            "Time to choose a winner!:",
+            msgs.PICK_WINNER,
             reply_markup=InlineKeyboardMarkup(buttons),
         )
 
 
 def _handle_round_end(content, game, chat_id, client, msg):
-    client.send_message(games_runner.get_chat_id(chat_id), "We have a winner card!:")
+    client.send_message(games_runner.get_chat_id(chat_id), msgs.ROUND_WINNER)
     client.send_message(
         games_runner.get_chat_id(chat_id),
         {game.get_card_from_picked(int(content))},
@@ -71,7 +72,7 @@ def start_command(client, message):
 
     client.send_message(
         message.chat.id,
-        f"{message.from_user.first_name} joined the game. /start when all players are joined",
+        message.from_user.first_name + msgs.JOINED_GAME,
     )
 
 
@@ -82,31 +83,30 @@ def game_round(client, message, game):
         if message.chat.id < 0
         else games_runner.get_chat_id(message.chat.id)
     )
-    client.send_message(chat_id, f"You have a new black card. X choose")
+    client.send_message(chat_id, msgs.PICK_BLACK)
     client.send_message(chat_id, f"***{game.get_black_card()}***")
     for user in games_runner.player_to_chat.keys():
         buttons = game.get_buttons(user)
         keyboard = InlineKeyboardMarkup(buttons)
-        print(keyboard)
-        client.send_message(user, "Time to choose.", reply_markup=keyboard)
+        client.send_message(user, msgs.PICK_WHITE, reply_markup=keyboard)
 
 
-@app.on_message(filters.command("start"))
+@app.on_message(filters.command(msgs.START_GAME_KEYWORD))
 def start_command(client, message):
     games_runner.add_player(message.chat.id, message.from_user.id)
     client.send_message(
-        message.chat.id, f"{message.from_user.first_name} joined the game."
+        message.chat.id, message.from_user.first_name + msgs.JOINED_GAME
     )
-    client.send_message(message.chat.id, f"Lets play the game!")
+    client.send_message(message.chat.id, msgs.START_GAME)
     game = games_runner.get_game(message.chat.id)
     user_id = message.from_user.id
     game_round(client, message, game)
 
 
-@app.on_message(filters.command("hi"))
+@app.on_message(filters.command(msgs.INTRODUCE_KEYWORD))
 def say_hi(client, message):
     user_id = message.from_user.id
-    client.send_message(user_id, "hi.")
+    client.send_message(user_id, msgs.REPLY_INTRODUCE)
 
 
 app.run()
